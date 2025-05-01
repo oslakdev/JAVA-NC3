41480cdf413d23ab6aa940ecd49b9713152d9ab2
Edieson M. Peneyra <68199172+oslakdev@users.noreply.github.com>
2025-05-01 14:26:27 +0000

    tasksheet1-1-1

--- PackageMain/Tasksheet_1_1_1.java
+++ PackageMain/Tasksheet_1_1_1.java
@@ -2,7 +2,7 @@
  * The HelloWorldApp class implements an application that
  * simply prints "Hello World!" to standard output.
  */
-class HelloWorldApp {
+class Tasksheet_1_1_1 {
     public static void main(String[] args) {
         System.out.println("Kamusta Mundo!"); // Display the string.
     }

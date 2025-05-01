ffefcaf403fc608b41fe164885e43a764ab6dd26
Edieson M. Peneyra <68199172+oslakdev@users.noreply.github.com>
2025-05-01 13:48:02 +0000

    aa

--- PackageMain/Main.java
+++ PackageMain/Main.java
@@ -1,7 +1,9 @@
-
-public class Main{
-    
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.println("Hello Main");
+        System.out.println("Kamusta Mundo!"); // Display the string.
     }
 }

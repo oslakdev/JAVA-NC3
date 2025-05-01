635b82e4957eee609ca8bda67097a24276174d05
Edieson M. Peneyra <68199172+oslakdev@users.noreply.github.com>
2025-05-01 18:58:39 +0000

    task2

---
+++ Task2.java
@@ -0,0 +1,16 @@
+public class Task2 {
+
+    public static void main(String[] args) {
+        char L = 'l';
+        byte zero = 0;
+        short e = 3;
+        int LL = 11;
+        float twozero = 2.0f;
+        boolean bool= true;
+        String output = "H" + e + LL + zero + " w" + zero + "r" + L + "d " + twozero +" " + bool;
+
+        System.out.println(output);
+
+    }
+    
+}

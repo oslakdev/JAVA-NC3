0057c8632251923de720ca9763883d984c7918b4
Edieson M. Peneyra <68199172+oslakdev@users.noreply.github.com>
2025-05-08 03:58:38 +0000

    task5

---
+++ PackageMain/Task5.java
@@ -0,0 +1,44 @@
+ /*Task5
+  * Check whose largest number in 3 inputs.
+    Create a program that takes in three input number from user
+    and output the largest number. If all numbers are equall print "all numbers are equal".
+  */
+
+
+import java.util.Scanner;
+
+public class Task5 {
+
+    public static void main(String[] args) {
+
+        Scanner scann = new Scanner(System.in);
+
+        int num1;
+        int num2;
+        int num3;
+        
+        System.out.print("Please enter number1: ");
+        int number1 = scann.nextInt();
+        num1 = number1;
+
+        System.out.print("Please enter number2: ");
+        int number2 = scann.nextInt();
+        num2 = number2;
+        
+        System.out.print("Please enter number3: ");
+        int number3 = scann.nextInt();
+        num3 = number3;
+
+        if (num1 > num2 && num1 > num3) {
+            System.out.println(num1 +" is the greatest");
+        } else if (num2 > num1 && num2 > num3) {
+            System.out.println(num2 +" is the greatest");
+        } else if (num3 > num1 && num3 > num2) {
+            System.out.println(num3 +" is the greatest");
+        }else if(num1 == num2 && num1 == num3 ){
+            System.out.println(num1 + " " + num2 +"" + "and " + num3 + " are all equal");
+        }
+    }
+
+   
+}

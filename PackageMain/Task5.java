ec7f1b16a30d1a7e8c85133384ed4ac34229d397
Edieson M. Peneyra <68199172+oslakdev@users.noreply.github.com>
2025-05-08 04:26:57 +0000

    update

--- PackageMain/Task5.java
+++ PackageMain/Task5.java
@@ -17,30 +17,37 @@ public class Task5 {
         int num2;
         int num3;
         
-        System.out.print("Please enter number1: ");
+        System.out.print("Please enter first number: ");
         int number1 = scann.nextInt();
         num1 = number1;
 
-        System.out.print("Please enter number2: ");
+        System.out.print("Please enter second number: ");
         int number2 = scann.nextInt();
         num2 = number2;
         
-        System.out.print("Please enter number3: ");
+        System.out.print("Please enter third number: ");
         int number3 = scann.nextInt();
         num3 = number3;
 
         scann.close();
 
         if (num1 > num2 && num1 > num3) {
-            System.out.println(num1 +" is the greatest");
+            System.out.println(num1 +" is the greatest number");
         } else if (num2 > num1 && num2 > num3) {
-            System.out.println(num2 +" is the greatest");
+            System.out.println(num2 +" is the greatest number");
         } else if (num3 > num1 && num3 > num2) {
-            System.out.println(num3 +" is the greatest");
+            System.out.println(num3 +" is the greatest number");
+        }else if (num1 == num2 && num1 > num3){
+            System.out.println(num1 +" and "+ num2 +" are equal and both greatest number");
+
+        }else if (num2 == num3 && num2 > num1){
+            System.out.println(num2 +" and "+ num3 +" are equal and both greatest number");
+
+        }else if (num3 == num1 && num3 > num2){
+            System.out.println(num3 +" and "+ num1 +" are equal and both greatest number");
+
         }else if(num1 == num2 && num1 == num3 ){
             System.out.println(num1 + " " + num2 +"" + "and " + num3 + " are all equal");
         }
     }
-
-   
 }

44bf0e816bb44a03ee4798514f3d950fada177e3
Edieson M. Peneyra <68199172+oslakdev@users.noreply.github.com>
2025-05-08 04:43:06 +0000

    update

--- PackageMain/Task5.java
+++ PackageMain/Task5.java
@@ -36,10 +36,13 @@ public class Task5 {
 
         if (num1 > num2 && num1 > num3) {
             System.out.println(num1 +" is the greatest number.");
+
         } else if (num2 > num1 && num2 > num3) {
             System.out.println(num2 +" is the greatest number.");
+
         } else if (num3 > num1 && num3 > num2) {
             System.out.println(num3 +" is the greatest number.");
+            
         }else if (num1 == num2 && num1 > num3){
             System.out.println(num1 +" and "+ num2 +" are equal and both the greatest number.");
 

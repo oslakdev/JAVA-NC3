3d7de443088a207b42975f27e34b774b84db543a
Edieson M. Peneyra <68199172+oslakdev@users.noreply.github.com>
2025-05-28 15:53:52 +0000

    task8

--- PackageMain/Task8.java
+++ PackageMain/Task8.java
@@ -1,50 +1,36 @@
+
+// Write a program featuring an integer variable arguments method. The method should return the sum of all parameters and, for each parameter, 
+            // the cumulative sum up to that point. For instance, given parameters (4, 5, 10), 
+            // the program should compute the total for the first parameter, such as 4 = (1+2+3+4), 
+            // and repeat this process for subsequent parameters.
+            
 public class Task8 {
 
-    //         Write a program featuring an integer variable arguments method. The method should return the sum of all parameters and, for each parameter, 
-    //         the cumulative sum up to that point. For instance, given parameters (4, 5, 10), 
-    //         the program should compute the total for the first parameter, such as 4 = (1+2+3+4), 
-    //         and repeat this process for subsequent parameters.
-             
+            
+    
+    private static int computeCumulativeSum(int n) {
+        return n * (n + 1) / 2;  
+    }
+
+    
+    public static int sumCumulativeSums(int... numbers) {
+        int totalSum = 0;
+        
+        System.out.println("Calculating cumulative sums for each input:");
+        for (int num : numbers) {
+            int currentCumulativeSum = computeCumulativeSum(num);
+            System.out.printf("the cumulative sum of 1 to %d is: %d\n", num, currentCumulativeSum);
+            totalSum += currentCumulativeSum;
+        }
+        
+        return totalSum;
+    }
+         
     
     public static void main(String[] args) {
-        //
-
+        // Example usage
+        int result = sumCumulativeSums(3, 5, 6);
+        System.out.println("The total cumulative sum of all is: " + result);
     }
     
-  
-    // public static void processNumbers(int... numbers) {
-    //     int totalSum = 0;
-    //     System.out.println("Processing numbers: " + arrayToString(numbers));
-        
-    //     // Calculate and display cumulative sum for each number
-    //     for (int num : numbers) {
-    //         int cumulativeSum = calculateCumulativeSum(num);
-    //         System.out.printf("Cumulative sum up to %d: %d%n", num, cumulativeSum);
-    //         totalSum += num;
-    //     }
-        
-    //     // Display the sum of all parameters
-    //     System.out.printf("Sum of all parameters: %d%n", totalSum);
-    //     System.out.println(); // Add blank line for separation
-    // }
-    
-   
-    // private static int calculateCumulativeSum(int n) {
-    //     // Using the formula for sum of first n natural numbers: n(n+1)/2
-    //     return n * (n + 1) / 2;
-    // }
-    
-  
-    // private static String arrayToString(int[] array) {
-    //     StringBuilder sb = new StringBuilder();
-    //     sb.append("[");
-    //     for (int i = 0; i < array.length; i++) {
-    //         sb.append(array[i]);
-    //         if (i < array.length - 1) {
-                sb.append(", ");
-            }
-//         }
-//         sb.append("]");
-//         return sb.toString();
-//     }
-// }
+}

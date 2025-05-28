81baa358c3d1ecca8de277dd4ef04287b00255bc
Edieson M. Peneyra <68199172+oslakdev@users.noreply.github.com>
2025-05-28 15:17:43 +0000

    task13

--- PackageMain/Car.java
+++
@@ -1,9 +0,0 @@
-public class TaskCar {
-
-    int numberOfDoors;
-    
-    public void displayDetails(super(make), String model, int year, int numberOfDoors){
-        this.numberOfDoors = numberOfDoors;
-
-
-}
